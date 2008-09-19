9854ca6321a90110be6b77f1237cac52398aaa36
Ryan Lowe <ryanlowe@gmail.com>
2008-09-18 20:35:42 -0400

    add jump_off baseline project

---
+++ config/initializers/basic.rb
@@ -0,0 +1,6 @@
+
+SITE_NAME = "RubyBuild"
+DOMAIN_NAME = "rubybuild.org"
+
+NOTIFIER_EMAIL = "#{SITE_NAME} <notifier@#{DOMAIN_NAME}>"
+INFO_EMAIL = "#{SITE_NAME} <info@#{DOMAIN_NAME}>"
---
+++ config/initializers/exception_notifier.rb
@@ -0,0 +1,4 @@
+# ExceptionNotifier plugin configuration
+ExceptionNotifier.exception_recipients = %w(ryanlowe@gmail.com)
+ExceptionNotifier.sender_address = %("RubyBuild" <exception@rubybuild.org>)
+ExceptionNotifier.email_prefix = ""
